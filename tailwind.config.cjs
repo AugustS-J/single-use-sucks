8f212be3fe854343cf946fa27375c82629f59123
Sofie <sofieulrich@gmail.com>
2024-04-02 10:54:00 +0200

    farver

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-  };
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+  theme: {
+    colors: {
+      primary: {
+        100: "#184B5C",
+      },
+
+      secondary: {
+        100: "#DCF0F1",
+      },
+
+      tertiary: {
+        100: "#A0D4D1",
+      },
+
+      highlight: {
+        100: "#F4F9FB",
+      },
+
+      accent: {
+        100: "#FF6E65",
+      },
+    },
+  },
+};
